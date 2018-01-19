1fee5d53808779f450c6dbc28ff90f6f5085c1e8
pedro <cruzrodriguezpedroj@gmail.com>
2018-01-19 13:56:54 -0600

    iconografia y estructura php

---
+++ recursos/head.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>vistas plataforma de criptomoneda</title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./js/bootstrap.min.js">
+  </head>
+  <body>
+      <script defer src="https://use.fontawesome.com/releases/v5.0.4/js/all.js"></script>
+      <header>
+        <nav class="navbar navbar-light bg-light">
+          <div class="col-md-8">
+          </div>
+          <div class="col-md-2">
+              <button type="button" class="btn btn-light"><i class="fas fa-sign-in-alt"></i> Iniciar sesion</button>
+          </div>
+          <div class="col-md-2">
+            <button type="button" class="btn btn-light">Registrarse</button>
+          </div>
+
+        </nav>
+      </header>
