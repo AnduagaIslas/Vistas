da9f1e2124687e86754a73b4deb22cb9480646cb
Jose Luis Anduaga Islas <anduagajl@gmail.com>
2018-01-19 14:51:15 -0600

    vista Asistencia

---
+++ Asistencia.php
@@ -0,0 +1,21 @@
+<?php
+include('recursos/head.php');
+ ?>
+ <link href="head.html" rel="import" />
+   <div class="row">
+     <div class="col-sm-6">
+         <h1>Asistencia</h1>
+     </div>
+
+     <div class="col-sm-6">
+      <div align="right">
+        <img src="imagenes/moneda.png" width="150" height="150">
+      </div>
+     </div>
+
+   </div>
+
+   
+<?php
+include('recursos/footer.php');
+ ?>
