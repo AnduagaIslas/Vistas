03e444478b92109d75b698936aac1aea12c08f94
pedro <cruzrodriguezpedroj@gmail.com>
2018-01-22 12:53:58 -0600

    modals iniciar sesion, registrarse

--- recursos/head.php
+++ recursos/head.php
@@ -16,26 +16,36 @@
           </div>
           <div class="col-3 col-sm-2 col-md-1">
             <!-- Button trigger modal -->
-            <button type="button" class=" btn btn-light" data-toggle="modal" data-target="#exampleModal">
+            <button type="button" class=" btn btn-light" data-toggle="modal" data-target="#iniciarModal">
             <i class="fas fa-sign-in-alt"></i> Iniciar
             </button>
 
             <!-- Modal -->
-            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal fade" id="iniciarModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog" role="document">
               <div class="modal-content">
                 <div class="modal-header">
-                  <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                  <h5 class="modal-title" id="exampleModalLabel">Iniciar Sesion</h5>
                   <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                   </button>
                 </div>
                 <div class="modal-body">
-                  ...
+                  <form>
+                    <div class="form-group">
+                      <label for="exampleInputEmail1">Nombre de usuario</label>
+                      <input type="" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nombre de usuario">
+                      <small id="emailHelp" class="form-text text-muted">Nombre de usuario unico e intransferible para uso de la plataforma</small>
+                    </div>
+                    <div class="form-group">
+                      <label for="exampleInputPassword1">Password</label>
+                      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    </div>
+
+                  </form>
                 </div>
                 <div class="modal-footer">
-                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                  <button type="button" class="btn btn-primary">Save changes</button>
+                    <button type="submit" class="btn btn-light">Entrar</button>
                 </div>
               </div>
             </div>
@@ -43,7 +53,63 @@
 
           </div>
           <div class="col-3 col-sm-2 col-md-1">
-            <button type="button" class="btn btn-light"><i class="fas fa-plus-circle"></i> Registrarse</button>
+            <!-- Button trigger modal -->
+            <button type="button" class=" btn btn-light" data-toggle="modal" data-target="#registrarseModal">
+            <i class="fas fa-sign-in-alt"></i> Registrarse
+            </button>
+
+            <!-- Modal -->
+            <div class="modal fade" id="registrarseModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Iniciar Sesion</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <form>
+                    <div class="form-row">
+                      <div class="form-group">
+                        <label for="inputAddress">Nombre de usuario</label>
+                        <input type="text" class="form-control" id="inputAddress" placeholder="Juan Hdez">
+                      </div>
+                    </div>
+                    <div class="form-row">
+                      <div class="form-group col-md-6">
+                        <label for="inputEmail4">Email</label>
+                        <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+                      </div>
+                    </div>
+                    <div class="form-row">
+                      <div class="form-group col-md-6">
+                        <label for="inputPassword4">Password</label>
+                        <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+                      </div>
+                      <div class="form-group col-md-6">
+                        <label for="inputPassword4">Confirm Password</label>
+                        <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+                      </div>
+                    </div>
+
+                    <div class="form-group">
+                      <div class="form-check">
+                        <input class="form-check-input" type="checkbox" id="gridCheck">
+                        <label class="form-check-label" for="gridCheck">
+                          Acepto terminos
+                        </label>
+                      </div>
+                    </div>
+
+                  </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-light">Registrar</button>
+                </div>
+              </div>
+            </div>
+            </div>
           </div>
 
         </nav>
