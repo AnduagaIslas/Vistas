defb8ef49151efd19fd0ca303f7a3454278f9758
pedro <cruzrodriguezpedroj@gmail.com>
2018-01-22 12:30:45 -0600

    modals helpers

--- index.php
+++ index.php
@@ -1,31 +1,7 @@
 <?php
 include('recursos/head.php');
 ?>
-<div class="row">
-  <div class="col-md-4 text-center ">
-    <img class="logotipo"src="img/logo.png" alt="">
-  </div>
-  <div class="col-md-8">
-    <ul class="nav menu">
-      <li class="nav-item">
-        <a class="nav-link" href="Pagos.php">PAGOS</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="Transacciones.php">TRANSACCIONES</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="Compras.php">COMPRAS</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="Fondo-Social.php">FONDO SOCIAL</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="Asistencia.php">ASISTENCIA</a>
-      </li>
 
-    </ul>
-  </div>
-</div>
 <br>
 <div class="row text-center">
   <div class="col-md-3">
@@ -49,7 +25,7 @@ include('recursos/head.php');
   </div>
 </div>
 <div class="row container-fluid">
-  <div class="col-3">
+  <div class="col-12 col-sm-3">
     <div class="list-group">
 
       <button type="button" class="list-group-item list-group-item-action">Contrato de renta mensual $1,200.00</button>
@@ -62,11 +38,12 @@ include('recursos/head.php');
     </div>
 
   </div>
-  <div class="col-1">
+  <div class="col-sm-1">
 
   </div>
-  <div class="col-8">
+  <div class="col-12 col-sm-8">
     <img src="img/grafica-normal.jpg" alt="">
+  </div>
 </div>
 <br>
 <br>
--- recursos/head.php
+++ recursos/head.php
@@ -3,11 +3,9 @@
   <head>
     <meta charset="utf-8">
     <title>vistas plataforma de criptomoneda</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/bootstrap.min.css">
     <link rel="stylesheet" href="./css/estilos.css">
-    <script type="text/javascript" src="./js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="./js/funciones.js"></script>
-    
   </head>
   <body>
 
@@ -49,4 +47,30 @@
           </div>
 
         </nav>
+        <div class="row">
+          <div class="col-md-4 text-center ">
+            <img class="logotipo"src="img/logo.png" alt="">
+          </div>
+          <div class="col-md-8">
+            <ul class="nav menu">
+              <li class="nav-item">
+                <a class="nav-link" href="Pagos.php">PAGOS</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="Transacciones.php">TRANSACCIONES</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="Compras.php">COMPRAS</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="Fondo-Social.php">FONDO SOCIAL</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="Asistencia.php">ASISTENCIA</a>
+              </li>
+
+            </ul>
+          </div>
+        </div>
+
       </header>
